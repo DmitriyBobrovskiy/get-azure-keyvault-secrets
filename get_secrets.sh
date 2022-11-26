39318a4be1fbc9039e58282165783621da95b819
Dmitrii Bobrovskii <Dmitrii.Bobrovskii@cloudmore.com>
2022-11-26 17:13:16 +0200

    remove extra quotes, fix logs

--- get_secrets.sh
+++ get_secrets.sh
@@ -3,19 +3,19 @@ keyVaultName=$1
 input=$2
 hideSecrets=$3
 
-echo "Getting secrets from $keyVaultName and hiding secrets: $hideSecrets"
+echo "::notice::Getting secrets from $keyVaultName and hiding secrets: $hideSecrets"
 
 while read -r line; do
     # running in background since we are getting secrets one by one
     # and running in foreground will take more time
     (
-        echo "Reading line: ::debug::$line"
+        echo "::debug::Reading line: $line"
         if [ -n "$line" ]; then
             envVariableName="${line%=*}"
             secretName="${line#*=}"
             echo "Environment variable name: $envVariableName, secret name: $secretName"
 
-            secretValue=$(az keyvault secret show --name "$secretName" --vault-name "$keyVaultName" --query value)
+            secretValue=$(az keyvault secret show --name "$secretName" --vault-name "$keyVaultName" --query value --output tsv)
             if [ "$hideSecrets" = true ]; then
                 echo "::add-mask::$secretValue"
                 echo "Secret name: $secretName value: $secretValue"
